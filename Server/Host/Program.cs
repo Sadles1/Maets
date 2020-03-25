6ad3a833ce50338ec7a58d72c89617ba7828881b
Sadles1 <snayper2034@gmail.com>
2020-03-25 09:50:47 +0500

    Clear Server

---
+++ Server/Host/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var host = new ServiceHost(typeof(Service.WCFService)))
+                {
+
+                    host.Open();
+                    Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}: Server start!");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
