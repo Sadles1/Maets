259da74e7093e0b2b2c19999ad131e5ed8fa8a83
p4shark <55730859+p4shark@users.noreply.github.com>
2020-04-06 21:40:17 +0500

    Add files via upload

--- Client/Client/Data/Views/Korzina.xaml.cs
+++ Client/Client/Data/Views/Korzina.xaml.cs
@@ -17,11 +17,11 @@ namespace Client
     /// <summary>
     /// Логика взаимодействия для ShopWindows.xaml
     /// </summary>
-    public partial class ShopWindows : Window
+    public partial class Korzina : Window
     {
         Service.Profile profile;
         DataProvider dp = new DataProvider();
-        public ShopWindows(Service.Profile profile)
+        public Korzina(Service.Profile profile)
         { 
             this.profile = profile;
             InitializeComponent();
@@ -29,10 +29,7 @@ namespace Client
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            imMainImage.Source = dp.GetImageFromByte(profile.MainImage);
-            lbLogin.Content = profile.Login;
-            lbName.Content = profile.Name;
-            Lv.ItemsSource = profile.Friends;
+
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
@@ -44,7 +41,9 @@ namespace Client
         }
         private void TbExit_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow.client.Disconnect(profile.ID);
+            ShopWindows MF = new ShopWindows(profile);
+           
+            MF.Show();
             this.Close();
         }
 
@@ -58,25 +57,14 @@ namespace Client
             }
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void Buy_Click(object sender, RoutedEventArgs e)
         {
-            Korzina buyProduct = new Korzina(profile);
-            buyProduct.Show();
-            this.Close();
-        }
-
-        private void BtnexitProfile_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow MF = new MainWindow();
-            MainWindow.client.Disconnect(profile.ID);
-            MF.Show();
-            this.Close();
-        }
-
-        private void BtnFakeProduct_Click(object sender, RoutedEventArgs e)
-        {
-            Product Pr = new Product();
-            Pr.Show();
+          
         }
     }
 }
